ffa2e7954dfd4cf12218ed85978d4a9c6b028ac6
Prasad Rajandran <prasadrajandran@gmail.com>
2022-10-10 00:36:33 -0400

    Update tests

--- tests/h.test.js
+++ tests/h.test.js
@@ -4,6 +4,16 @@ const testData = {
   'Parses element name': () => {
     return h('div', h('p'));
   },
+  'Parses element name/attrs inside backticks': () => {
+    return h(
+      `
+      div
+        class="btn btn-primary"
+        style="font-size: 25px;"
+    `,
+      h('p')
+    );
+  },
   'Parses element name and attributes as a single argument': () => {
     return h(
       'div',
@@ -18,15 +28,13 @@ const testData = {
       h(
         `div style="font-size: 10px; display: absolute"`,
         {
-          style: {
+          $style: {
             flexDirection: 'column',
           },
-          dataAttrs: {
+          $data: {
             expectedPattern: '[a-z]',
           },
-          attrs: {
-            role: 'button',
-          },
+          role: 'button',
           class: 'btn btn-primary',
           id: 'mybtn',
         },
@@ -41,12 +49,10 @@ const testData = {
         h(
           `div role="panel" style="font-size: 10px; display: absolute"`,
           {
-            style: {
+            $style: {
               fontSize: '20px',
             },
-            attrs: {
-              role: 'button',
-            },
+            role: 'button',
           },
           'Label!'
         )
@@ -61,43 +67,86 @@ const testData = {
 };
 
 describe('h()', () => {
-  test('Only the element name is required', () => {
-    expect(() => h('div')).not.toThrowError();
-    expect(() => h()).toThrowError();
-  });
-
-  test('Grabbing reference to element via callback', () => {
-    let element = null;
-
-    h('button', { ref: (el) => (element = el) });
-
-    expect(element instanceof HTMLButtonElement).toEqual(true);
-    expect(element instanceof HTMLDivElement).toEqual(false);
-  });
-
-  test('Applying events', () => {
-    const onclick = jest.fn(() => {});
-    const onabort = () => {};
-    let elementRef = null;
-
-    h('button', {
-      events: { onclick, onabort },
-      ref: (btn) => (elementRef = btn),
+  describe('First argument', () => {
+    test('Is required', () => {
+      expect(() => h('div')).not.toThrowError();
+      expect(() => h()).toThrowError('First argument to h() is invalid: ');
     });
 
-    expect(elementRef.onclick).toEqual(onclick);
-    expect(elementRef.onclick).not.toEqual(onabort);
-    expect(elementRef.onabort).toEqual(onabort);
-    expect(elementRef.onabort).not.toEqual(onclick);
+    test('Will throw if it produces an invalid node', () => {
+      expect(() => h('dsadsa')).toThrowError(
+        'First argument to h() is invalid: '
+      );
+    });
 
-    expect(onclick).toBeCalledTimes(0);
-    elementRef.click();
-    expect(onclick).toBeCalledTimes(1);
+    test('Will throw if it produces more than one node', () => {
+      expect(() => h('div <p>')).toThrowError(
+        'First argument to h() should not contain more than one element: '
+      );
+    });
+
+    test('Attribute values have to be strings and event values have to be functions', () => {
+      expect(() => h('div', { id: 1 })).toThrowError(
+        'Value of "id" attribute has to be a string'
+      );
+      expect(() => h('div', { class: true })).toThrowError(
+        'Value of "class" attribute has to be a string'
+      );
+      expect(() => h('div', { role: 5 })).toThrowError(
+        'Value of "role" attribute has to be a string'
+      );
+      expect(() => h('div', { onclick: 5 })).toThrowError(
+        'Value of "onclick" has to be a function'
+      );
+      expect(() => h('div', { $style: { fontSize: 10 } })).toThrowError(
+        'Value of "fontSize" CSS style property has to be a string'
+      );
+      expect(() => h('div', { $data: { isReady: false } })).toThrowError(
+        'Value of "isReady" data attribute has to be a string'
+      );
+      expect(() => h('div', { $aria: { label: true } })).toThrowError(
+        'Value of "label" ARIA attribute has to be a string'
+      );
+    });
   });
 
-  for (const [testName, fn] of Object.entries(testData)) {
-    test(testName, () => {
-      expect(fn()).toMatchSnapshot();
+  describe('Second argument', () => {
+    test('Grabbing reference to element via callback', () => {
+      let element = null;
+
+      h('button', { $ref: (el) => (element = el) });
+
+      expect(element instanceof HTMLButtonElement).toEqual(true);
+      expect(element instanceof HTMLDivElement).toEqual(false);
     });
-  }
+
+    test('Applying events', () => {
+      const onclick = jest.fn(() => {});
+      const onabort = () => {};
+      let elementRef = null;
+
+      h('button', {
+        onclick,
+        onabort,
+        $ref: (btn) => (elementRef = btn),
+      });
+
+      expect(elementRef.onclick).toEqual(onclick);
+      expect(elementRef.onclick).not.toEqual(onabort);
+      expect(elementRef.onabort).toEqual(onabort);
+      expect(elementRef.onabort).not.toEqual(onclick);
+
+      expect(onclick).toBeCalledTimes(0);
+      elementRef.click();
+      expect(onclick).toBeCalledTimes(1);
+    });
+  });
+
+  describe('Snapshots', () => {
+    for (const [testName, fn] of Object.entries(testData)) {
+      test(testName, () => {
+        expect(fn()).toMatchSnapshot();
+      });
+    }
+  });
 });
