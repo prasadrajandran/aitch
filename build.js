2dd66486d61b1fa3a8191ff4287ad5988ac91176
Prasad Rajandran <prasadrajandran@gmail.com>
2023-04-19 23:27:16 -0400

    Update build script - should bundle instead

--- build.js
+++ build.js
@@ -6,14 +6,12 @@ const srcDir = path.resolve(__dirname, 'src');
 const distDir = path.resolve(__dirname, 'dist');
 const entryPoints = [
   path.resolve(srcDir, 'index.ts'),
-  path.resolve(srcDir, 'h.ts'),
-  path.resolve(srcDir, 'repeat.ts'),
-  path.resolve(srcDir, 'style.ts'),
+  path.resolve(srcDir, 'directives.ts'),
 ];
 const buildOptions = {
   entryPoints,
   outdir: distDir,
-  bundle: false,
+  bundle: true,
   minify: true,
   sourcemap: true,
   format: 'esm',
